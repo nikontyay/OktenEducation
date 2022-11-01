85024d437ac9c535d159e2ee65d1359dd1c70f7d
nikontyay <danilnikonov27@gmail.com>
2022-11-01 12:17:57 +0300

    Update

---
+++ JS/lesson1/main.js
@@ -0,0 +1,35 @@
+let a = 'hello-';
+
+let b ='owu.';
+
+let c = 'com';
+
+let d = '.ua';
+console.log(a + b + c + d)
+
+const e = 1;
+console.log(e);
+
+const f = 10;
+console.log(f);
+
+const g = -999;
+console.log(g);
+
+const h = 123;
+console.log(h);
+
+const i = 3.14;
+console.log(i);
+
+let j = 2.7;
+console.log(j);
+
+let k = 16;
+console.log(k);
+
+let l = 10 > 11;
+console.log(l);
+
+m = 300 === 300;
+console.log(m);
