edf36cec19cb1fa2c4cda6d3dc2c03f4c633bd02
nikontyay <danilnikonov27@gmail.com>
2022-11-24 20:32:20 +0200

    Update

--- JS/lesson6/main.js
+++ JS/lesson6/main.js
@@ -50,6 +50,11 @@
 
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
 // let nums = [10,8,-7,55,987,-1011,0,1050,0];
+// // let str = nums.map(value => value.toString());
+// // let str = nums.map(value => String(value));
+// // let str = nums.map(value => value + '')
+// console.log(str);   
+
 
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
 //     let nums = [11,21,3];
@@ -68,25 +73,38 @@
 // })
 // console.log(sortNums2)
 
+// let nums = [11,21,3];
+//
+// const sortNums = (direction, arr) => {
+//     if (direction === 'ascending') {
+//         arr.sort ((a,b) => a - b);
+//     }
+//     else if (direction === 'descending') {
+//         arr.sort ((a,b) => b -a );
+//     }
+//     return arr;
+// }
+// console.log(sortNums('descending',nums));
+
 
 
 
 //
 //
 // - є масив
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
-// -- відсортувати його за спаданням за monthDuration
-
-let sorted = coursesAndDurationArray.sort((a,b) => {
-   return  b.monthDuration - a.monthDuration;
-})
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// // -- відсортувати його за спаданням за monthDuration
+//
+// let sorted = coursesAndDurationArray.sort((a,b) => {
+//    return  b.monthDuration - a.monthDuration;
+// })
 // console.log(sorted);
 
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
@@ -97,17 +115,12 @@ let sorted = coursesAndDurationArray.sort((a,b) => {
 // console.log(filtered)
 
 // Second Way
-let filtered = coursesAndDurationArray.filter((value) =>{
-    return value.monthDuration > 5;
-})
-console.log(filtered);
+// let filtered = coursesAndDurationArray.filter((value) =>{
+//     return value.monthDuration > 5;
+// })
+// console.log(filtered);
 
-// описати колоду карт
-// - знайти піковий туз
-// - всі шістки
-// - всі червоні карти
-// - всі буби
-// - всі трефи від 9 та більше
+
 //
 //
 // {
@@ -116,6 +129,41 @@ console.log(filtered);
 //     color:'', // 'red','black'
 // }
 //
+const suits = [
+    {cardSuit: 'heart', color: 'red'},
+    {cardSuit: 'diamond', color: 'red'},
+    {cardSuit: 'spade', color: 'black'},
+    {cardSuit: 'clubs', color: 'black'}
+]
+
+const cardNames = [6,7,8,9,10,'jack','queen','king','ace'];
+const deck = [];
+
+for (const suit of suits) {
+    for (const cardName of cardNames) {
+        const card = {
+            cardSuit: suit.cardSuit,
+            name: cardName,
+            color: suit.color
+        }
+        deck.push(card);
+    }
+    
+}
+console.log(deck);
+
+// описати колоду карт
+// - знайти піковий туз
+let filtered = deck.filter(function (value){
+    return value.cardSuit === 'spade' && value.name === 'ace'
+})
+console.log(filtered)
+// - всі шістки
+// - всі червоні карти
+// - всі буби
+// - всі трефи від 9 та більше
+
+
 //
 // Додатково по reduce
 // Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
