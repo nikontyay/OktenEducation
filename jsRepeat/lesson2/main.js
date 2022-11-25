d28162f07a3b51b605d45421cba8da08ffaa62ec
nikontyay <danilnikonov27@gmail.com>
2022-11-25 15:37:46 +0200

    Update

---
+++ jsRepeat/lesson2/main.js
@@ -0,0 +1,146 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let arr = [];
+//
+// arr[0]= 1;
+// arr[1]= 2;
+// arr[2] = 3;
+// arr[3] = 'hello';
+// arr[4] = 'world';
+// arr[5] = 'my';
+// arr[6] = 'name';
+// arr[7] = 'is';
+// arr[8] = true;
+// arr[9]= false;
+//
+// console.log(arr)
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let harryPotter = {
+//     title: 'Harry Potter',
+//     pageCount: '300',
+//     genre: 'fantastic'
+// };
+// console.log(harryPotter);
+//
+// let lordOfRings = {
+//     title: 'Lord of Rings',
+//     pageCount: '400',
+//     genre: 'fantastic'
+// };
+// console.log(lordOfRings);
+//
+// let hobbit = {
+//     title: 'Hobbit',
+//     pageCount: '500',
+//     genre: 'fantastic'
+// };
+// console.log(hobbit);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// let harryPotter = {
+//     title: 'Harry Potter',
+//     pageCount: '300',
+//     genre: 'fantastic'
+// };
+// harryPotter.author = [
+//     {
+//         name: 'Rouling',
+//         age: 41
+//     }]
+// console.log(harryPotter);
+//
+// let lordOfRings = {
+//     title: 'Lord of Rings',
+//     pageCount: '400',
+//     genre: 'fantastic',
+// };
+// lordOfRings.author = [
+//     {
+//         name: 'Tolkin',
+//         age: 45
+//     }
+// ];
+// console.log(lordOfRings);
+//
+// let hobbit = {
+//     title: 'Hobbit',
+//     pageCount: '500',
+//     genre: 'fantastic',
+// };
+// hobbit.author = [
+//     {
+//         name: 'Tolkin',
+//         age: 45
+//     }
+// ]
+// console.log(hobbit);
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+// let users = [];
+//
+// users[0] = {
+//     name: 'Vasya',
+//     username: 'loginvasya',
+//     password: 'passwordvasya'
+// };
+//
+// users[1] = {
+//     name: 'Petya',
+//     username: 'loginpetya',
+//     password: 'passwordpetya'
+// };
+//
+// users[2] = {
+//     name: 'Anna',
+//     username: 'loginanna',
+//     password: 'passwordanna'
+// }
+// console.log(users);
+
+
+// Practice ----------------------------------------
+
+// let array = [
+//     [123,22,33],
+//     [234,345],
+//     [345]
+// ]
+//
+// console.log(array[0][1])
+//
+// let innerArray = array[2][0];
+// console.log(innerArray);
+//
+// //-----------------------------------------------------
+//
+// let user1 = {
+//     name: 'Danylo',
+//     age: 24,
+//     skills: ['html', 'css', 'js'],
+//     wife: {
+//         name: 'Katya',
+//         age: '21'
+//     }
+// }
+//
+// console.log(user1);
+// console.log(user1.name);
+// console.log(user1.skills[0]);
+//
+// //
+// // //-------------------------------------------------------
+// //
+// // let users2 = [
+// //     {id:1, name:'Petya', age:20, status:true},
+// //     {id:2, name:'Vasya', age:21, status:false},
+// //     {id:3, name:'Katya', age:22, status:true},
+// //     {id:4, name:'Danya', age:24, status:false}
+// // ];
+// //
+// // console.log(users2[0].age)
+// //
+// // let user0 = users2[2];
+// // console.log(users2);
+// // console.log(users2[3].name);
+// // console.log(user0.name);
