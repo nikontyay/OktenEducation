59e49ff60717e37c449877a0e167b56b02b67e2c
nikontyay <danilnikonov27@gmail.com>
2022-12-18 22:55:23 +0200

    Update

--- jsRepeat/MiniProject/main.js
+++ jsRepeat/MiniProject/main.js
@@ -9,16 +9,19 @@
 
 let container = document.createElement('div');
 document.body.appendChild(container);
+container.classList.add('container')
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then((request) => request.json())
     .then((value) => {
             for (const element of value) {
                 let userDiv = document.createElement('div');
+                userDiv.classList.add('userDiv');
                 container.appendChild(userDiv);
                 userDiv.innerText = `${element.id} --- ${element.name}`;
                 let userBtn = document.createElement('button');
-                container.appendChild(userBtn);
+                userDiv.appendChild(userBtn);
+                userBtn.classList.add('userBtn');
                 userBtn.innerText = 'My page';
                 userBtn.onclick = function () {
                     window.location = 'user_details.html?id=' + element.id;
