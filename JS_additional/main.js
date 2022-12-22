e246ab1b38e626413b58415ca186a1e88cc7fbc3
nikontyay <danilnikonov27@gmail.com>
2022-12-22 21:02:04 +0200

    Update

--- JS_additional/main.js
+++ JS_additional/main.js
@@ -113,5 +113,73 @@
 // console.log(person6.status.completed); // false
 // console.log(person5.status.completed); // true
 
+// Функція - це об'єкт; console.dir(Функція) - відобразить тіло функції
+// function myFn(a, b) {
+//     let c;
+//     a = a + 1;
+//     c = a + b;
+//     return c
+// }
+// console.log(myFn(10, 20));
 
+// Створення функції, яка мутує оригінальний об'єкт (не рекомендується, але є випадки коли це необхідно)
+// const personOne = {
+//     name: 'Bob',
+//     age: 21
+// }
+// function increasePersonAge(somePerson) {
+//     somePerson.age += 1
+//     return somePerson
+// }
+//
+// console.log(increasePersonAge(personOne));
 
+// Створення копії об'єкта всередині функції, щоб не мутувати оригінальний об'єкт
+// const personOne = {
+//     name: 'Bob',
+//     age: 21
+// }
+//
+// function increasePersonAge(somePerson) {
+//     const someUpdatedPerson = Object.assign({}, somePerson)
+//     someUpdatedPerson.age += 1
+//     return someUpdatedPerson
+// }
+//
+// const personTwo = increasePersonAge(personOne);
+// console.log(personTwo);
+// console.log(personOne);
+
+// Callback функція - коли аргументом однієї функції є інша функція (одна функція викликає іншу функцію)
+// Зміст callback функції, що вона викликається всередині іншої функції
+// function printMyName() {
+//     console.log('Danylo')
+// }
+//
+// setTimeout(printMyName,1000);
+
+// Правила функцій:
+// 1. Називати функції виходячи з результату функції (щоб назва була логічна)
+// 2. Одна функція повинна виконувати одну дію
+// 3. Не рекомендується змінювати зовнішні щодо функції змінні (яка мутує оригінальний об'єкт) - але є випадки коли це необхідно
+// Якщо нема return - то функція поверне undefined
+
+// Глобальні та локальні області видимості
+// Якщо в Глобальній області видимості та Локальній області видимості "Один" будуть створені змінні "b", 
+// то при роботі в Локальній області "Один" ми будемо працювати зі змінною, яка створена в Локальній області видимості "Один". 
+// Якщо в Локальній області видимості "Два" не була створена змінна "b", 
+// то при роботі з нею в Локальній області видимості "Два", ми будемо брати значення змінної "b" з Глобальної області видимості.
+let a; //глобальна змінна
+let b; //глобальна змінна
+
+function myFn() {
+    let b //локальна змінна
+    a = true;
+    b = 10;
+    console.log(b);
+}
+
+myFn();
+
+console.log(a); // true
+console.log(b); // undefined
